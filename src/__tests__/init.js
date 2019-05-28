537a295b00b2d83f94522a0e38ff275271f49dea
Paul Gadi <paulgadi@gmail.com>
2019-05-28 16:56:00 +0800

    outdated test due to HOC change

--- src/__tests__/init.js
+++ src/__tests__/init.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
+// import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
 import App from '../App';
-import withReactLowdb from '../package';
 
 Enzyme.configure( {adapter: new Adapter() } );
 
@@ -14,10 +13,11 @@ describe('React tests.', () => {
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  it('loads db into props', async () => {
-    const WrappedComponent = withReactLowdb(App);
-    const wrapper = shallow(<WrappedComponent/>);
-
-    expect(Object.keys(wrapper.props()).includes('db')).toBe(true);
-  });
+  //
+  // it('loads db into props', async () => {
+  //   const WrappedComponent = withReactLowdb(App);
+  //   const wrapper = shallow(<WrappedComponent/>);
+  //
+  //   expect(Object.keys(wrapper.props()).includes('db')).toBe(true);
+  // });
 });
