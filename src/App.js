2c6ebcd7145b6ef01f7c5a192acbdda7bfcc248d
Paul Gadi <paulgadi@gmail.com>
2019-05-28 15:24:33 +0800

    added react-json-view to demo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
-
-// import withReactLowdb from 'react-lowdb'; // use this when installed with npm
+import ReactJson from 'react-json-view';
+// import withReactLowdb from 'react-lowdb'; // replace './package' with 'react-lowdb'
 import withReactLowdb from './package';
 
 class App extends Component {
+
+  defaultDBFields = {
+    selectedButton: null
+  }
+
   constructor(props) {
     super(props);
+
+    // initialize lowdb with default object if none found
+    props.db.defaults(this.defaultDBFields).write();
   }
 
   componentDidMount() {
-    
+    this.loadStateFromDB();
+  }
+
+  loadStateFromDB() {
+    var dbJson = this.props.db.getState();
+    this.setState({
+      ...dbJson
+    })
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <h1>react-lowdb demo</h1>
+        <p>This component&apos;s state is edittable on the&nbsp;
+        <a href="https://www.npmjs.com/package/react-json-view">React JSON View</a>
+        &nbsp;below.
+        </p>
+        <p>Changes are saved to LocalStorage as the state changes. You can try
+        refreshing the page to see the data being saved.</p>
+        <ReactJson theme="monokai" src={this.state}/>
       </div>
     );
   }
